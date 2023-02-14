d7c336e2ee98853c4ce8976e6c98cb1131f3985e
Nazmun2012 <nazmunl2012@gmail.com>
2023-02-14 12:13:58 -0500

    committing test 2branchA

--- src/Practice/P.java
+++ src/Practice/P.java
@@ -5,7 +5,8 @@ public class P {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String s= "i can do it.";
+		// count words
+		/*String s= "i can do it.";
 		int count=1;
 		
 		for(int i =0; i<=s.length()-1; i++) {
@@ -15,8 +16,34 @@ public class P {
 		}
 		
 		
-		//System.out.println(i +" "+ j);
-	}
+		System.out.println(count);*/
+		
+					
+			
+		/*int arr []= {1,2,3};
+		int reminder = 1;
+		int reverse=0;
+		
+		for(int i=arr.length-1; i>=0; i--) {
+			
+			reminder= arr[i]%10;
+			reverse = reverse*10+reminder;
+			arr[i] = arr[i]/10;
+		}System.out.println(reverse);*/
+		
+		
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=5; j>=i;j--) {
+				System.out.print(" ");	
+			}
+			for(int k=1;k<=i; k++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		
 	
+	}
 
 }
